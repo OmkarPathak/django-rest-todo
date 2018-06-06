a9d3cb79a73bf3d5fc48034774ed7fbd751ce00d
Omkar Pathak <omkarpathak27@gmail.com>
2018-06-06 20:48:31 +0530

    Modified acc to jwt specs

--- todo_rest/todo_rest/settings.py
+++ todo_rest/todo_rest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,13 +130,22 @@ STATIC_URL = '/static/'
 #     ]
 # }
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': (
-#         'rest_framework.permissions.IsAuthenticated',
-#     ),
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-#         'rest_framework.authentication.SessionAuthentication',
-#         'rest_framework.authentication.BasicAuthentication',
-#     ),
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+
+# JWT_AUTH = { 
+#     'JWT_AUTH_HEADER_PREFIX': 'JWT',
+#     'JWT_EXPIRATION_DELTA': datetime.timedelta(seconds=300)
 # }
+
+JWT_AUTH = { 
+    'JWT_AUTH_HEADER_PREFIX': 'Bearer',
+}
--- todo_rest/todo_rest/urls.py
+++ todo_rest/todo_rest/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
+# from rest_framework_jwt.views import refresh_jwt_token
+# from rest_framework_jwt.views import verify_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('todo.urls'))
+    path('api/', include('todo.urls')),
+    path('api/auth/login/', obtain_jwt_token),
+    # path('auth-jwt-refresh/', refresh_jwt_token),
+    # path('auth-jwt-verify/', verify_jwt_token),
 ]
