45f44b68fde54428a0f95bb558769c4e4c1b2224
Omkar Pathak <omkarpathak27@gmail.com>
2018-06-11 12:54:51 +0530

    Major frontend changes and compledted edit apu

--- angular_code/frontend2/src/app/app.component.ts
+++ angular_code/frontend2/src/app/app.component.ts
@@ -6,16 +6,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css', "../../node_modules/bootstrap/dist/css/bootstrap.min.css",],
   providers: [UserService]
 })
 export class AppComponent{
   site_title = 'My first Django, Angular REST API';
   register;
   tasks;
-
+  id;
   title:string;
   description:string;
+  etitle:string;
+  edescription:string;
   url= 'http://127.0.0.1:8000/api/tasks/';
 
   constructor(private userService:UserService, private http: HttpClient){
@@ -26,62 +28,30 @@ export class AppComponent{
     );
   }
 
-  show_all_notes(){
-    this.userService.get_all_tasks().subscribe(
-      result=>{
-        this.tasks = result;
-      }
-    );
-  }
-
-  // retrieve_tasks(){
-  //   return this.http.get('http://127.0.0.1:8000/api/tasks');
-  // }
-
   onSubmit(notes: Task){
-    this.http.post('http://127.0.0.1:8000/api/tasks/add/', notes).subscribe(status=> console.log(JSON.stringify(status)));
-
-    //this.tasks = this.retrieve_tasks()
+    this.http.post('http://127.0.0.1:8000/api/tasks/add/', notes).subscribe(status=>this.tasks.push(status)); 
   }
 
-  // btnClick= function () {
-  //   this.router.navigateByUrl('http://127.0.0.1:8000/api/tasks/5/delete/');
-  // };
+  onEdit(notes: Task){
+    this.http.put('http://127.0.0.1:8000/api/tasks/' + this.id + '/edit/', notes).subscribe(status=>{
+      this.userService.get_all_tasks().subscribe(
+        result=>{
+          this.tasks = result;
+        }
+      );
+    });
+  }
 
+  btnClick(id){
+    this.http.delete('http://127.0.0.1:8000/api/tasks/' + id + '/delete/').subscribe(status=>this.tasks.splice(this.tasks.indexOf(status)));
+  }
 
-  //newly added ...start
-  // delete_task(task: Person){
-  //   return this.http.get('http://127.0.0.1:8000/api/tasks/5/delete/');
-  // }
-  //...end
-
-  
-
-
-
+  editTask(id){
+    this.http.get('http://127.0.0.1:8000/api/tasks/' + id + '/').subscribe(status=>{this.id=status.id; this.etitle=status.title;this.edescription=status.description});
+  }
 }
 
 export interface Task {
   title: string;
   description: string;
 }
-
-// export class AppComponent {
-//   title: 'My first Django REST'
-//   private url: 'http://127.0.0.1:8000/api/tasks/'
-//   public apps: Applications[];
-  
-//   constructor(http: HttpClient) {
-//       http.get(this.url).subscribe(result => {
-//           this.apps = result.json() as Applications[];
-//       }, error => console.error(error));
-//   }}
-  
-//   interface Applications {
-//      id: number;
-//      type: string;
-//      port: string;
-//      baseUrl: string;
-//      architecture: string;
-//      protocol: string;
-//      serveur: string; }
